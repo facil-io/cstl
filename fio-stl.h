842f3ca71f433e9e2a09772d27b1851c6367331e
Bo <bo@bowild.com>
2020-09-11 14:44:16 +0300

    auto-generated

--- fio-stl.h
+++ fio-stl.h
@@ -9775,7 +9775,7 @@ print_help:
     type = (char *)FIO_CLI_STRING__TYPE_I;
     switch ((intptr_t)pos[1]) {
     case FIO_CLI_PRINT__TYPE_I:
-      fprintf(stderr, "          \t");
+      fprintf(stderr, "          \t   ");
       fio___cli_print_line(pos[0], parser->argv[0]);
       fprintf(stderr, "\n");
       pos += 2;
@@ -10929,6 +10929,488 @@ FIO_SOCK - cleanup
 ***************************************************************************** */
 #undef FIO_SOCK
 #endif
+/* *****************************************************************************
+Copyright: Boaz Segev, 2019-2020
+License: ISC / MIT (choose your license)
+
+Feel free to copy, use and enjoy according to the license provided.
+***************************************************************************** */
+#ifndef H___FIO_CSTL_INCLUDE_ONCE_H /* Development inclusion - ignore line */
+#define FIO_ATOMIC                  /* Development inclusion - ignore line */
+#define FIO_POLL                    /* Development inclusion - ignore line */
+#define FIO_POLL_DEV                /* Development inclusion - ignore line */
+#include "000 header.h"             /* Development inclusion - ignore line */
+#include "003 atomics.h"            /* Development inclusion - ignore line */
+#include "100 mem.h"                /* Development inclusion - ignore line */
+#endif                              /* Development inclusion - ignore line */
+#ifdef FIO_POLL_DEV                 /* Development inclusion - ignore line */
+#include "201 array.h"              /* Development inclusion - ignore line */
+#include "202 hashmap.h"            /* Development inclusion - ignore line */
+#endif                              /* Development inclusion - ignore line */
+
+/* *****************************************************************************
+
+
+
+
+
+
+
+
+
+
+      A packet based data stream for storing / buffering endless data.
+
+
+
+
+
+
+
+
+
+
+***************************************************************************** */
+#if defined(FIO_POLL) && !defined(H___FIO_POLL___H)
+#define H___FIO_POLL___H
+
+#if !FIO_HAVE_UNIX_TOOLS
+#warning "POSIX is required for the fio_poll API."
+#endif
+#include <poll.h>
+
+#ifndef FIO_POLL_HAS_UDATA_COLLECTION
+/* A unique `udata` per fd (true)? or a global `udata` (false)?*/
+#define FIO_POLL_HAS_UDATA_COLLECTION 1
+#endif
+
+/* *****************************************************************************
+Polling API
+***************************************************************************** */
+
+/** the `fio_poll_s` type should be considered opaque. */
+typedef struct fio_poll_s fio_poll_s;
+
+typedef struct {
+  /** callback for when data is availabl in the incoming buffer. */
+  void (*on_data)(int fd, void *udata);
+  /** callback for when the outgoing buffer allows a call to `write`. */
+  void (*on_ready)(int fd, void *udata);
+  /** callback for closed connections and / or connections with errors. */
+  void (*on_close)(int fd, void *udata);
+} fio_poll_settings_s;
+
+#define FIO_POLL_INIT(on_data_func, on_ready_func, on_close_func)              \
+  {                                                                            \
+    .settings =                                                                \
+        {                                                                      \
+            .on_data = on_data_func,                                           \
+            .on_ready = on_ready_func,                                         \
+            .on_close = on_close_func,                                         \
+        },                                                                     \
+    .lock = FIO_LOCK_INIT                                                      \
+  }
+
+#ifndef FIO_REF_CONSTRUCTOR_ONLY
+/** Creates a new polling object / queue. */
+FIO_IFUNC fio_poll_s *fio_poll_new(fio_poll_settings_s settings);
+#define fio_poll_new(...) fio_poll_new((fio_poll_settings_s){__VA_ARGS__})
+
+/** Frees the polling object and its resources. */
+FIO_IFUNC int fio_poll_free(fio_poll_s *p);
+#endif /* FIO_REF_CONSTRUCTOR_ONLY */
+
+/** Destroys the polling object, freeing its resources. */
+FIO_IFUNC void fio_poll_destroy(fio_poll_s *p);
+
+/**
+ * Adds a file descriptor to be monitored, adds events to be monitored or
+ * updates the monitored file's `udata`.
+ *
+ * Possible flags are: `POLLIN` and `POLLOUT`. Other flags may be set but might
+ * be ignored.
+ *
+ * Monitoring mode is always one-shot. If an event if fired, it is removed from
+ * the monitoring state.
+ *
+ * Returns -1 on error.
+ */
+SFUNC int
+fio_poll_monitor(fio_poll_s *p, int fd, void *udata, unsigned short flags);
+
+/**
+ * Reviews if any of the monitored file descriptors has any events.
+ *
+ * `timeout` is in milliseconds.
+ *
+ * Returns the number of events called.
+ *
+ * Polling is thread safe, but has different effects on different threads.
+ *
+ * Adding a new file descriptor from one thread while polling in a different
+ * thread will not poll that IO untill `fio_poll_review` is called again.
+ */
+SFUNC int fio_poll_review(fio_poll_s *p, int timeout);
+
+/**
+ * Stops monitoring the specified file descriptor, returning its udata (if any).
+ */
+SFUNC void *fio_poll_forget(fio_poll_s *p, int fd);
+
+/* *****************************************************************************
+
+
+
+
+
+
+
+
+                          Poll Monitoring Implementation
+
+
+
+
+
+
+
+
+***************************************************************************** */
+
+/* *****************************************************************************
+Poll Monitoring Implementation - The polling type(s)
+***************************************************************************** */
+
+#define FIO_RISKY_HASH
+#define FIO_MAP_TYPE uint32_t
+#define FIO_MAP_NAME fio___poll_index
+#ifndef FIO_STL_KEEP__
+#define FIO_STL_KEEP__ 1
+#endif
+#include __FILE__
+#define FIO_ARRAY_TYPE           struct pollfd
+#define FIO_ARRAY_NAME           fio___poll_fds
+#define FIO_ARRAY_TYPE_CMP(a, b) (a.fd == b.fd)
+#ifndef FIO_STL_KEEP__
+#define FIO_STL_KEEP__ 1
+#endif
+#include __FILE__
+#if FIO_POLL_HAS_UDATA_COLLECTION
+#define FIO_ARRAY_TYPE void *
+#define FIO_ARRAY_NAME fio___poll_udata
+#ifndef FIO_STL_KEEP__
+#define FIO_STL_KEEP__ 1
+#endif
+#include __FILE__
+#endif /* FIO_POLL_HAS_UDATA_COLLECTION */
+
+struct fio_poll_s {
+  fio_poll_settings_s settings;
+  fio___poll_index_s index;
+  fio___poll_fds_s fds;
+#if FIO_POLL_HAS_UDATA_COLLECTION
+  fio___poll_udata_s udata;
+#else
+  void *udata;
+#endif /* FIO_POLL_HAS_UDATA_COLLECTION */
+  fio_lock_i lock;
+};
+
+/* *****************************************************************************
+When avoiding the `udata` array
+***************************************************************************** */
+#if !FIO_POLL_HAS_UDATA_COLLECTION
+FIO_IFUNC void fio___poll_udata_destroy(void **pu) { *pu = NULL; }
+FIO_IFUNC void **fio___poll_udata_push(void **pu, void *udata) {
+  if (udata)
+    *pu = udata;
+  return pu;
+}
+FIO_IFUNC void **
+fio___poll_udata_set(void **pu, int32_t pos, void *udata, void **ignr) {
+  if (udata)
+    *pu = udata;
+  return pu;
+  (void)ignr;
+  (void)pos;
+}
+FIO_IFUNC void **fio___poll_udata2ptr(void **pu) { return pu; }
+FIO_IFUNC void *fio___poll_udata_get(void **pu, int32_t pos) {
+  return *pu;
+  (void)pos;
+}
+#endif /* FIO_POLL_HAS_UDATA_COLLECTION */
+/* *****************************************************************************
+Poll Monitoring Implementation - inlined static functions
+***************************************************************************** */
+
+/* do we have a constructor? */
+#ifndef FIO_REF_CONSTRUCTOR_ONLY
+/* Allocates a new object on the heap and initializes it's memory. */
+FIO_IFUNC fio_poll_s *fio_poll_new FIO_NOOP(fio_poll_settings_s settings) {
+  fio_poll_s *p = (fio_poll_s *)FIO_MEM_REALLOC_(NULL, 0, sizeof(*p), 0);
+  if (p) {
+    *p = (fio_poll_s) {
+      .settings = settings, .lock = FIO_LOCK_INIT, .index = FIO_MAP_INIT,
+      .fds = FIO_ARRAY_INIT,
+#if FIO_POLL_HAS_UDATA_COLLECTION
+      .udata = FIO_ARRAY_INIT,
+#endif
+    };
+  }
+  return p;
+}
+/* Frees any internal data AND the object's container! */
+FIO_IFUNC int fio_poll_free(fio_poll_s *p) {
+  fio_poll_destroy(p);
+  FIO_MEM_FREE_(p, sizeof(*p));
+  return 0;
+}
+#endif /* FIO_REF_CONSTRUCTOR_ONLY */
+
+/** Destroys the polling object, freeing its resources. */
+FIO_IFUNC void fio_poll_destroy(fio_poll_s *p) {
+  if (p) {
+    fio___poll_index_destroy(&p->index);
+    fio___poll_fds_destroy(&p->fds);
+    fio___poll_udata_destroy(&p->udata);
+    p->lock = FIO_LOCK_INIT;
+  }
+}
+
+/* *****************************************************************************
+Poll Monitoring Implementation - possibly externed functions.
+***************************************************************************** */
+#ifdef FIO_EXTERN_COMPLETE
+
+/* mock event */
+FIO_SFUNC void fio___poll_ev_mock(int fd, void *udata) {
+  (void)fd;
+  (void)udata;
+}
+
+FIO_IFUNC int
+fio___poll_monitor(fio_poll_s *p, int fd, void *udata, unsigned short flags) {
+  uint32_t pos = fio___poll_index_get(&p->index, fd, 0);
+  struct pollfd *i = fio___poll_fds2ptr(&p->fds);
+  if (i && i[pos].fd == fd)
+    goto edit_existing;
+  if (i && i[pos].fd == -1)
+    goto renew_monitoring;
+  /* insert new entry */
+  i = fio___poll_fds_push(&p->fds, (struct pollfd){.fd = fd, .events = flags});
+  if (!i)
+    return -1;
+  pos = (uint32_t)(i - fio___poll_fds2ptr(&p->fds));
+  if (!fio___poll_udata_push(&p->udata, udata)) {
+    fio___poll_fds_pop(&p->fds, NULL);
+    return -1;
+  }
+  fio___poll_index_set(&p->index, fd, pos, NULL);
+  return 0;
+
+edit_existing:
+  /* pos is correct, we are updating a value */
+  i[pos].events |= flags;
+  if (udata)
+    fio___poll_udata_set(&p->udata, (int32_t)pos, udata, NULL);
+  return 0;
+
+renew_monitoring:
+  i[pos].fd = fd;
+  i[pos].events = flags;
+  i[pos].revents = 0;
+  fio___poll_udata_set(&p->udata, (int32_t)pos, udata, NULL);
+  return 0;
+}
+
+/**
+ * Adds a file descriptor to be monitored, adds events to be monitored or
+ * updates the monitored file's `udata`.
+ *
+ * Possible flags are: `POLLIN` and `POLLOUT`. Other flags may be set but might
+ * be ignored.
+ *
+ * Returns -1 on error.
+ */
+SFUNC int
+fio_poll_monitor(fio_poll_s *p, int fd, void *udata, unsigned short flags) {
+  int r = -1;
+  if (!p || fd == -1)
+    return r;
+  flags &= POLLIN | POLLOUT | POLLPRI;
+  fio_lock(&p->lock);
+  r = fio___poll_monitor(p, fd, udata, flags);
+  fio_unlock(&p->lock);
+  return r;
+}
+
+/**
+ * Reviews if any of the monitored file descriptors has any events.
+ *
+ * Returns the number of events called.
+ *
+ * Polling is thread safe, but has different effects on different threads.
+ *
+ * Adding a new file descriptor from one thread while polling in a different
+ * thread will not poll that IO untill `fio_poll_review` is called again.
+ */
+SFUNC int fio_poll_review(fio_poll_s *p, int timeout) {
+  int r = -1;
+  int c = 0;
+  size_t to_copy = 0;
+  fio_poll_s cpy;
+  if (!p)
+    return r;
+
+  /* move all data to a copy (thread safety) */
+  fio_lock(&p->lock);
+  cpy = *p;
+  p->index = (fio___poll_index_s)FIO_MAP_INIT;
+  p->fds = (fio___poll_fds_s)FIO_ARRAY_INIT;
+#if FIO_POLL_HAS_UDATA_COLLECTION
+  p->udata = (fio___poll_udata_s)FIO_ARRAY_INIT;
+#endif /* FIO_POLL_HAS_UDATA_COLLECTION */
+
+  fio_unlock(&p->lock);
+
+  /* move if conditions out of the loop */
+  if (!cpy.settings.on_data)
+    cpy.settings.on_data = fio___poll_ev_mock;
+  if (!cpy.settings.on_ready)
+    cpy.settings.on_ready = fio___poll_ev_mock;
+  if (!cpy.settings.on_close)
+    cpy.settings.on_close = fio___poll_ev_mock;
+
+  /* poll the array */
+  struct pollfd *const fds_ary = fio___poll_fds2ptr(&cpy.fds);
+  size_t const len = fio___poll_fds_count(&cpy.fds);
+  void **const ud_ary = fio___poll_udata2ptr(&cpy.udata);
+#if FIO_POLL_HAS_UDATA_COLLECTION
+#define FIO___POLL_UDATA_GET(index) ud_ary[(index)]
+#else
+#define FIO___POLL_UDATA_GET(index) ud_ary[0]
+#endif
+  r = poll(fds_ary, len, timeout);
+
+  /* process events */
+  if (r > 0) {
+    for (size_t i = 0; i < len; ++i) {
+      if ((fds_ary[i].revents & POLLIN) || (fds_ary[i].revents & POLLPRI)) {
+        cpy.settings.on_data(fds_ary[i].fd, FIO___POLL_UDATA_GET(i));
+      }
+      if ((fds_ary[i].revents & POLLOUT)) {
+        cpy.settings.on_ready(fds_ary[i].fd, FIO___POLL_UDATA_GET(i));
+      }
+      if ((fds_ary[i].revents & POLLHUP) || (fds_ary[i].revents & POLLERR) ||
+          (fds_ary[i].revents & POLLNVAL)) {
+        cpy.settings.on_close(fds_ary[i].fd, FIO___POLL_UDATA_GET(i));
+        fds_ary[i].events = 0; /* never retain events after closure / error */
+      }
+      fds_ary[i].events &= ~fds_ary[i].revents;
+      if (fds_ary[i].events) {
+        /* unfired events await */
+        fds_ary[to_copy].fd = fds_ary[i].fd;
+        fds_ary[to_copy].events = fds_ary[i].events;
+        FIO___POLL_UDATA_GET(to_copy) = FIO___POLL_UDATA_GET(i);
+        ++to_copy;
+      }
+      /* any more events? */
+      if (c++ >= r)
+        break;
+    }
+  } else
+    to_copy = fio___poll_fds_count(&cpy.fds);
+
+  /* insert all unfired events back to the (thread safe) queue */
+  fio_lock(&p->lock);
+  if (!c && !fio___poll_index_count(&p->index)) {
+    /* no editing was performed, it's possible to move the data set as is */
+    *p = cpy;
+    cpy = (fio_poll_s)FIO_POLL_INIT(NULL, NULL, NULL);
+  } else {
+    for (size_t i = 0; i < to_copy; ++i) {
+      fio___poll_monitor(
+          p, fds_ary[i].fd, FIO___POLL_UDATA_GET(i), fds_ary[i].events);
+    }
+  }
+  fio_unlock(&p->lock);
+
+  /* cleanup memory */
+  fio___poll_index_destroy(&cpy.index);
+  fio___poll_fds_destroy(&cpy.fds);
+  fio___poll_udata_destroy(&cpy.udata);
+  return r;
+#undef FIO___POLL_UDATA_GET
+}
+
+/**
+ * Stops monitoring the specified file descriptor, returning its udata (if any).
+ */
+SFUNC void *fio_poll_forget(fio_poll_s *p, int fd) {
+  void *old = NULL;
+  if (!p || fd == -1)
+    return old;
+  fio_lock(&p->lock);
+  uint32_t pos = fio___poll_index_get(&p->index, fd, 0);
+  if (fio___poll_fds_get(&p->fds, pos).fd == fd) {
+    /* pos is correct (index 0 could have been a false positive) */
+    fio___poll_index_remove(&p->index, fd, 0, NULL);
+    fio___poll_fds2ptr(&p->fds)[(int32_t)pos].fd = -1;
+    old = fio___poll_udata_get(&p->udata, (int32_t)pos);
+  }
+  fio_unlock(&p->lock);
+  return old;
+}
+/* *****************************************************************************
+Poll Monitoring Testing?
+***************************************************************************** */
+#ifdef FIO_TEST_CSTL
+FIO_SFUNC void FIO_NAME_TEST(stl, poll)(void) {
+  fprintf(stderr,
+          "* testing file descriptor monitoring (setup / cleanup only).\n");
+  fio_poll_s p = FIO_POLL_INIT(NULL, NULL, NULL);
+  short events[4] = {POLLOUT, POLLIN, POLLOUT | POLLIN, POLLOUT | POLLIN};
+  for (int i = 128; i; --i) {
+    fio_poll_monitor(&p, i, (void *)(uintptr_t)i, events[(i & 3)]);
+  }
+  for (int i = 128; i; --i) {
+    if ((i & 3) == 3) {
+      fio_poll_forget(&p, i);
+    }
+  }
+  for (int i = 128; i; --i) {
+    size_t pos = fio___poll_index_get(&p.index, i, 0);
+    if ((i & 3) == 3) {
+      FIO_ASSERT(fio___poll_fds_get(&p.fds, pos).fd != i, "fd wasn't removed?");
+      FIO_ASSERT((int)(uintptr_t)fio___poll_udata_get(&p.udata, pos) != i,
+                 "udata value wasn't removed?");
+      continue;
+    }
+    FIO_ASSERT(fio___poll_fds_get(&p.fds, pos).fd == i,
+               "index value [%zu] doesn't match fd (%d != %d)",
+               pos,
+               fio___poll_fds_get(&p.fds, pos).fd,
+               i);
+    FIO_ASSERT(fio___poll_fds_get(&p.fds, pos).events == events[(i & 3)],
+               "events value isn't setup correctly");
+    FIO_ASSERT((int)(uintptr_t)fio___poll_udata_get(&p.udata, pos) == i,
+               "udata value isn't setup correctly");
+  }
+  fio_poll_destroy(&p);
+}
+
+#endif /* FIO_TEST_CSTL */
+/* *****************************************************************************
+Module Cleanup
+***************************************************************************** */
+
+#endif /* FIO_EXTERN_COMPLETE */
+#ifndef FIO_STL_KEEP__
+#undef FIO_POLL_HAS_UDATA_COLLECTION
+#endif
+#endif /* FIO_POLL */
+#undef FIO_POLL
 /* *****************************************************************************
 Copyright: Boaz Segev, 2019-2020
 License: ISC / MIT (choose your license)
@@ -13947,6 +14429,37 @@ FIO_SFUNC void FIO_NAME_TEST(stl, FIO_ARRAY_NAME)(void) {
       FIO_ASSERT(!(*(char *)&d.va[d.i + 1]),
                  "array each error (didn't stop in time?).");
     }
+#if FIO_ARRAY_TYPE_DESTROY_SIMPLE
+    {
+      FIO_NAME(FIO_ARRAY_NAME, destroy)(a);
+      size_t max_items = 63;
+      FIO_ARRAY_TYPE tmp[64];
+      for (size_t i = 0; i < max_items; ++i) {
+        memset(tmp + i, i + 1, sizeof(*tmp));
+      }
+      for (size_t items = 0; items <= max_items; items = ((items << 1) | 1)) {
+        FIO_LOG_DEBUG2("* testing the FIO_ARRAY_EACH macro with %zu items.",
+                       items);
+        size_t i = 0;
+        for (i = 0; i < items; ++i)
+          FIO_NAME(FIO_ARRAY_NAME, push)(a, tmp[i]);
+        i = 0;
+        FIO_ARRAY_EACH(FIO_ARRAY_NAME, a, pos) {
+          FIO_ASSERT(!memcmp(tmp + i, pos, sizeof(*pos)),
+                     "FIO_ARRAY_EACH pos is at wrong index %zu != %zu",
+                     (size_t)(pos - FIO_NAME2(FIO_ARRAY_NAME, ptr)(a)),
+                     i);
+          ++i;
+        }
+        FIO_ASSERT(i == items,
+                   "FIO_ARRAY_EACH macro count error - didn't review all "
+                   "items? %zu != %zu ",
+                   i,
+                   items);
+        FIO_NAME(FIO_ARRAY_NAME, destroy)(a);
+      }
+    }
+#endif
     /* test destroy */
     FIO_NAME(FIO_ARRAY_NAME, destroy)(a);
     FIO_ASSERT(!FIO_NAME(FIO_ARRAY_NAME, count)(a),
@@ -21069,6 +21582,10 @@ static int ary____test_was_destroyed = 0;
 #define FIO_PTR_UNTAG(p)               fio___dynamic_types_test_untag(((uintptr_t)p))
 #include __FILE__
 
+/* test all defaults */
+#define FIO_ARRAY_NAME ary3____test
+#include __FILE__
+
 /* *****************************************************************************
 Hash Map / Set - test
 ***************************************************************************** */
@@ -21405,6 +21922,7 @@ Socket helper and Stream testing
 #define FIO_SOCK
 #define FIO_STREAM
 #define FIO_SIGNAL
+#define FIO_POLL
 #include __FILE__
 
 /* *****************************************************************************
@@ -21709,6 +22227,7 @@ TEST_FUNC void fio_test_dynamic_types(void) {
   fprintf(stderr, "===============\n");
   FIO_NAME_TEST(stl, ary____test)();
   FIO_NAME_TEST(stl, ary2____test)();
+  FIO_NAME_TEST(stl, ary3____test)();
   fprintf(stderr, "===============\n");
   fio___dynamic_types_test___map_test();
   fprintf(stderr, "===============\n");
@@ -21723,6 +22242,8 @@ TEST_FUNC void fio_test_dynamic_types(void) {
   FIO_NAME_TEST(stl, stream)();
   fprintf(stderr, "===============\n");
   FIO_NAME_TEST(stl, signal)();
+  fprintf(stderr, "===============\n");
+  FIO_NAME_TEST(stl, poll)();
   fprintf(stderr, "===============\n");
   /* test memory allocator that initializes memory to zero */
   FIO_NAME_TEST(FIO_NAME(stl, fio_mem_test_safe), mem)();
--- fio-stl.md
+++ fio-stl.md
@@ -2649,6 +2649,144 @@ int fio_sock_open_unix(const char *address, int is_client, int nonblock);
 Creates a new Unix socket and binds it to a local address.
 
 -------------------------------------------------------------------------------
+## Basic IO Polling
+
+IO polling using the portable `poll` POSIX function is another area that's of common need and where many solutions are required.
+
+The facil.io standard library provides a persistent polling container for evented management of small IO (file descriptor) collections using the "one-shot" model.
+
+"One-Shot" means that once a specific even has "fired" (occurred), it will no longer be monitored (unless re-submitted). If the same file desciptor is waiting on multiple event, only those events that occurred will be removed from the monitored collection.
+
+There's no real limit on the number of file descriptors that can be monitored, except possible system limits that the system may impose on the `poll` system call. However, performance will degrade significantly as the ratio between inactive vs. active IO objects being monitored increases.
+
+It is recommended to use a system specific polling "engine" (`epoll` / `kqueue`) if polling thousands of persistent file descriptors.
+
+By defining `FIO_POLL` on a POSIX system, the following functions will be defined.
+
+### `FIO_POLL` API
+
+
+#### `fio_poll_s`
+
+```c
+typedef struct fio_poll_s fio_poll_s;
+```
+
+The `fio_poll_s` type should be considered opaque and should **not** be accessed directly.
+
+#### `FIO_POLL_INIT`
+
+```c
+#define FIO_POLL_INIT(on_data_func, on_ready_func, on_close_func)              \
+  {                                                                            \
+    .settings =                                                                \
+        {                                                                      \
+            .on_data = on_data_func,                                           \
+            .on_ready = on_ready_func,                                         \
+            .on_close = on_close_func,                                         \
+        },                                                                     \
+    .lock = FIO_LOCK_INIT                                                      \
+  }
+```
+
+A `fio_poll_s` object initialization macro.
+
+#### `fio_poll_new`
+
+```c
+fio_poll_s *fio_poll_new(fio_poll_settings_s settings);
+/* named argument support */
+#define fio_poll_new(...) fio_poll_new((fio_poll_settings_s){__VA_ARGS__})
+```
+
+Creates a new polling object / queue.
+
+The settings arguments set the `on_data`, `on_ready` and `on_close` callbacks:
+
+```c
+typedef struct {
+  /** callback for when data is availabl in the incoming buffer. */
+  void (*on_data)(int fd, void *udata);
+  /** callback for when the outgoing buffer allows a call to `write`. */
+  void (*on_ready)(int fd, void *udata);
+  /** callback for closed connections and / or connections with errors. */
+  void (*on_close)(int fd, void *udata);
+} fio_poll_settings_s;
+```
+
+Returns NULL on error (no memory).
+
+#### `fio_poll_free`
+
+```c
+int fio_poll_free(fio_poll_s *p);
+```
+
+Frees the polling object and its resources.
+
+#### `fio_poll_destroy`
+
+```c
+void fio_poll_destroy(fio_poll_s *p);
+```
+
+Destroys the polling object, freeing its resources.
+
+#### `fio_poll_monitor`
+
+```c
+int fio_poll_monitor(fio_poll_s *p, int fd, void *udata, unsigned short flags);
+```
+
+Adds a file descriptor to be monitored, adds events to be monitored or updates the monitored file's `udata`.
+
+Possible flags are: `POLLIN` and `POLLOUT`. Other flags may be set but might be ignored.
+
+Monitoring mode is always one-shot. If an event if fired, it is removed from the monitoring state.
+
+Returns -1 on error.
+
+#### `fio_poll_review`
+
+```c
+int fio_poll_review(fio_poll_s *p, int timeout);
+```
+
+Reviews if any of the monitored file descriptors has any events.
+
+`timeout` is in milliseconds.
+
+Returns the number of events called.
+
+**Note**:
+
+Polling is thread safe, but has different effects on different threads.
+
+Adding a new file descriptor from one thread while polling in a different thread will not poll that IO untill `fio_poll_review` is called again.
+
+#### `fio_poll_forget`
+
+```c
+void *fio_poll_forget(fio_poll_s *p, int fd);
+```
+
+Stops monitoring the specified file descriptor even if some of it's event's hadn't occurred just yet, returning its `udata` (if any).
+
+### `FIO_POLL` Compile Time Macros
+
+#### `FIO_POLL_HAS_UDATA_COLLECTION`
+
+```c
+#ifndef FIO_POLL_HAS_UDATA_COLLECTION
+#define FIO_POLL_HAS_UDATA_COLLECTION 1
+#endif
+```
+
+When set to true (the default value), the `udata` value is unique per file descriptor, using an array of `udata` values.
+
+When false, a global `udata` is used and it is updated whenever a `udata` value is supplied (`NULL` values are ignored).
+
+-------------------------------------------------------------------------------
 ## Data Stream Container
 
 Data Stream objects solve the issues that could arise when `write` operations don't write all the data (due to OS buffering). 
