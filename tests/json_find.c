2ff1636a0ac2535b3e28d8d71fa498365039f472
Bo <bo@bowild.com>
2021-01-10 17:11:53 +0300

    use macro in example

--- tests/json_find.c
+++ tests/json_find.c
@@ -17,21 +17,21 @@ int main(int argc, char const *argv[]) {
 
   // open and read the JSON fiile
   FIOBJ json = fiobj_str_new();
-  FIO_LOG_DEBUG2("attempting open:%s\n", fio_cli_unnamed(0));
+  FIO_LOG_DEBUG2("attempting open:%s", fio_cli_unnamed(0));
   fio_str_info_s result = fiobj_str_readfile(json, fio_cli_unnamed(0), 0, 0);
   FIO_ASSERT(result.buf, "Couldn't open file %s", fio_cli_unnamed(0));
 
   // parse the JSON data
-  FIO_LOG_DEBUG2("attempting to parse:\n%s\n", fiobj_str2cstr(json).buf);
+  FIO_LOG_DEBUG2("attempting to parse:\n%s", fiobj_str2cstr(json).buf);
   size_t consumed = 0;
   FIOBJ data = fiobj_json_parse(fiobj_str2cstr(json), &consumed);
   FIO_ASSERT(data != FIOBJ_INVALID, "couldn't parse data.");
   fiobj_free(json); /* we don't need the JSON file content anymore. */
 
   // re-format the JSON - example (replacing key-value pairs with arrays)
-  json = fiobj_json_find(data,
-                         (fio_str_info_s){.buf = (char *)fio_cli_unnamed(1),
-                                          .len = strlen(fio_cli_unnamed(1))});
+  json = fiobj_json_find2(data,
+                          (char *)fio_cli_unnamed(1),
+                          strlen(fio_cli_unnamed(1)));
   fiobj_dup(json);
   fiobj_free(data); /* we don't need the original object structure */
   data = fiobj2json(FIOBJ_INVALID, json, 1);
