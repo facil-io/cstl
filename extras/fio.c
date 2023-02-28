7012cfa328d94e79ed921adb03999645e83ef3ea
Bo <bo@bowild.com>
2023-02-28 02:27:02 +0200

    improve comments and readability

--- extras/fio.c
+++ extras/fio.c
@@ -5,21 +5,11 @@
  * with the project or separately to be used as a dynamic / static library.
  */
 #ifndef FIO_EXTERN_COMPLETE
-#define FIO_EXTERN_COMPLETE
+#define FIO_EXTERN_COMPLETE 99 /* a value above 1 == "non single-use" */
 #endif
 #include "fio.h"
 
 size_t fio_version_major(void) { return FIO_VERSION_MAJOR; }
 size_t fio_version_minor(void) { return FIO_VERSION_MINOR; }
 size_t fio_version_patch(void) { return FIO_VERSION_PATCH; }
-#if defined(FIO_VERSION_BUILD)
-const char *fio_version_build(void) { return FIO_VERSION_BUILD; }
-#else
-const char *fio_version_build(void) { return NULL; }
-#endif
 char *fio_version_string(void) { return FIO_VERSION_STRING; }
-
-size_t fio_version(void) {
-  return ((size_t)FIO_VERSION_MAJOR << 20) | ((size_t)FIO_VERSION_MINOR << 10) |
-         ((size_t)FIO_VERSION_PATCH);
-}
--- extras/fio.h
+++ extras/fio.h
@@ -1,20 +1,13 @@
 #ifndef H___INCLUDE_FIO_EVERYTHING___H
 /*
- * Allows the use of the facil.io C STL as a library.
+ * To use the facil.io C STL as a static library, we compile can it as a
+ * separate unit.
  *
  * This header includes all the macros and static functions, the compiled C file
- * will include all published functions.
+ * will include all the function definitions to be used as a static library.
  */
 #define FIO_EVERYTHING
-#define FIO_EXTERN
-
-// #define FIO_MEMORY_DISABLE 1
-// #define FIO_USE_THREAD_MUTEX 1
-// #define FIO_POLL_ENGINE FIO_POLL_ENGINE_POLL
-
-#ifdef DEBUG
-#define FIO_LEAK_COUNTER 1
-#endif
+#define FIO_EXTERN 99 /* a value above 1 == "non single-use" */
 
 #include "fio-stl/include.h"
 
@@ -24,24 +17,17 @@ size_t fio_version_major(void);
 size_t fio_version_minor(void);
 /** Returns the patch version number. */
 size_t fio_version_patch(void);
-/** Returns the build version string. */
-const char *fio_version_build(void);
 /** Returns the version number as a string. */
 char *fio_version_string(void);
-/** Returns the complete version number in 32 bit format. */
-size_t fio_version(void);
 
+/** This validates that the facil.io C STL library semantic version matches .*/
 FIO_SFUNC void fio_version_validate(void) {
   FIO_ASSERT(fio_version_major() == FIO_VERSION_MAJOR &&
                  FIO_VERSION_MINOR >= fio_version_minor(),
-             "facil.io C STL library version mismatch! (%d.x.x != %d.x.x)"
+             "facil.io C STL library version mismatch! (%s ! ~= %s)"
              "\n\tplease recompile to manage breaking changes.",
-             fio_version_major(),
-             FIO_VERSION_MAJOR);
+             fio_version_string(),
+             FIO_VERSION_STRING);
 }
-
-#define FIO_VERSION_NUMBER                                                     \
-  (((size_t)FIO_VERSION_MAJOR << 20) | ((size_t)FIO_VERSION_MINOR << 10) |     \
-   ((size_t)FIO_VERSION_PATCH))
 
 #endif /* H___INCLUDE_FIO_EVERYTHING___H */
